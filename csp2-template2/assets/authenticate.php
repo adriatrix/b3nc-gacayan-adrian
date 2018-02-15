c93ee94bc9ae173464fcff2c1f8277516ef5d6e6
adriatrix <adrian@gacayan.com>
2018-02-15 20:30:55 +0800

    changed authenticate php for sha1

--- csp2-template2/assets/authenticate.php
+++ csp2-template2/assets/authenticate.php
@@ -2,28 +2,39 @@
 
 session_start();
 
+require '../connect.php';
+
 $userName = $_POST['username'];
-$passWord = $_POST['password'];
+$passWord = sha1($_POST['password']);
 
 // echo $userName . ' ' . $passWord;
 
-$file = file_get_contents('users.json');
-$users = json_decode($file, true);
+// $file = file_get_contents('users.json');
+// $users = json_decode($file, true);
+
+$sql = "SELECT * FROM users WHERE username = '$username'";
+$result = mysqli_query($conn, $sql);
+$user = mysqli_fetch_assoc($result);
 
 // var_dump($users);
 
 $isLogInSuccessful = false;
 
-foreach ($users as $user) {
+// foreach ($users as $user) {
+if (mysql_num_rows($user) > 0) {
+  // while ($user = mysqli_fetch_assoc($users)) {
     // echo $user['username'] . ' ' . $user['password'];
+    // extract($user);
 
     if ($userName == $user['username'] && $passWord == $user['password']) {
         // echo 'Login was successful.';
         $isLogInSuccessful = true;
         $_SESSION['current_user'] = $user['username'];
         $_SESSION['role'] = $user['role'];
-        break;
+        // break;
     }
+} else {
+  alert("user not found");
 }
 
 if ($isLogInSuccessful) {
