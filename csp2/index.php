de801278199815e4ef8e5b8b29dbd188dd24de1a
adriatrix <adrian@gacayan.com>
2018-02-21 20:33:36 +0800

    csp2 init

---
+++ csp2/index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+  function getTitle() {
+    echo 'Your one stop shop for Funko Pops!';
+  }
+
+  include 'partials/head.php';
+
+  include 'partials/header.php';
+
+ ?>
+
+ <main class="my-wrapper">
+
+   <h1>Index Page</h1>
+
+   <button class="button is-primary" name="button">Button</button>
+
+ </main>
+
+<?php
+
+  include 'partials/footer.php';
+
+  include 'partials/foot.php';
+
+?>
---
+++ csp2/partials/footer.php
@@ -0,0 +1,3 @@
+<footer class="my-footer">
+	<p>Copyright &copy; 2018 by Adrian Gacayan &amp; The Pop Stop PH</p>
+</footer>
