2544e6148616bf7d4b836304d61aee18db8b7bd8
Adrian Gacayan <adrian@gacayan.com>
2018-03-06 17:01:22 -0600

    Figuring out sql for creating item

---
+++ csp2/assets/creatingitem.php
@@ -0,0 +1,66 @@
+<?php
+
+require '../connect.php';
+
+// $id =  intval($_POST['id']);
+$name = $_POST['name'];
+// $image =  $_POST['image'];
+$price = intval($_POST['price']);
+$stock = $_POST['stock'];
+$description = $_POST['description'];
+
+$rarity = $_POST['rarity'];
+$series = $_POST['series'];
+$brand = $_POST['brand'];
+$sub_brand = $_POST['sub-brand'];
+//
+// echo $name . ' ' . $price;
+// echo $stock . ' ' . $description . ' ' . $rarity;
+// echo $series . ' ' . $brand . ' ' . $sub_brand;
+
+$sql = "SELECT * FROM rarities WHERE rarity = '".$rarity."'";
+$result = mysqli_query($conn, $sql);
+while ($rarity = mysqli_fetch_assoc($result)) {
+  extract($rarity);
+  $rarity_id = $id;
+}
+
+$sql = "SELECT * FROM serials WHERE series = '".$series."'";
+$result = mysqli_query($conn, $sql);
+while ($series = mysqli_fetch_assoc($result)) {
+  extract($series);
+  $serial_id = $id;
+}
+
+$sql = "SELECT * FROM brands WHERE brand = '".$brand."'";
+$result = mysqli_query($conn, $sql);
+
+while ($brand = mysqli_fetch_assoc($result)) {
+  extract($brand);
+  $brand_id = $id;
+}
+
+$sql = "SELECT * FROM sub_brands WHERE sub_brand = '".$sub_brand."'";
+$result = mysqli_query($conn, $sql);
+
+while ($sub_brand = mysqli_fetch_assoc($result)) {
+  extract($sub_brand);
+  $sub_brand_id = $id;
+}
+
+
+$sql = "INSERT INTO items (name, price, stock, description, rarity_id, serial_id, brand_id, sub_brand_id) VALUES ('".$name."','".$price."','".$stock."','".$description."','".$rarity_id."','".$serial_id."','".$brand_id."','".$sub_brand_id."')";
+var_dump($sql);
+
+$result = mysqli_query($conn, $sql);
+
+// check if current new user was successful
+// if ($result)
+//   header('location: ../catalogue.php');
+// else
+//   die('Error: ' .$sql. '<br>' . mysqli_error($conn));
+//
+
+mysqli_close($conn);
+
+// header('location: ../catalogue.php');
