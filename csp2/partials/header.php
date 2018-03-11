2e194f7bb580c91d98bf3ffd3df8a449af9310e0
Adrian <adrian@gacayan.com>
2018-03-11 21:44:03 +0800

    redone navbar

--- csp2/partials/header.php
+++ csp2/partials/header.php
@@ -1,120 +1,154 @@
-<!-- <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="">
-      <img src="assets/img/small-banner.png" alt="The Pop Stop PH - your one stop shop for Funko Pops!" width="230" height="100">
-    </a>
-
-    <div class="navbar-burger burger" data-target="navMenu">
-      <span></span>
-      <span></span>
-      <span></span>
+<nav class="navbar is-dark is-fixed-top">
+  <div class="container">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="index.php">
+        <img src="assets/img/logo.jpg" alt="Logo">
+      </a>
+      <?php
+      if(isset($_SESSION['current_user'])) {
+        echo'
+        <div class="navbar-item is-hidden-desktop is-pulled-left">
+        <div class="field is-grouped">
+        <a class="button is-inverted is-info is-small" href="profile.php">
+        <span class="icon">
+        <i class="fas fa-user"></i>
+        </span>
+        <span>' . ucfirst($_SESSION['current_user']) . '</span>
+        </a>
+        <a class="button is-danger is-small" href="signout.php">
+        <span class="icon">
+        <i class="fas fa-sign-out-alt"></i>
+        </span>
+        </a>
+        </div>
+        </div>
+        ';
+      } else {
+        echo '
+        <div class="navbar-item is-hidden-desktop">
+        <div class="field is-grouped is-outlined">
+        <p class="control">
+        <a class="button is-primary is-inverted" href="signin.php">
+        <strong>Sign In</strong>
+        </a>
+        </p>
+        </div>
+        </div>
+        ';
+      }
+      ?>
+      <span class="navbar-item is-hidden-desktop is-pulled-right">
+        <a class="button is-white is-outlined" href="basket.php">
+          <span class="icon">
+            <i class="fas fa-shopping-basket"></i>
+          </span>
+          <span></span>
+          <span id="basket-badge">
+            <span class="my-badge">
+              <?php
+              if (isset($_SESSION['basket_count'])) {
+                echo ''.$_SESSION['basket_count'].'';
+              }
+              ?>
+            </span>
+          </span>
+        </a>
+      </span>
+      <span class="navbar-burger burger" data-target="navbarMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
     </div>
-  </div>
 
-  <div class="navbar-menu" id="navMenu">
-    <div class="navbar-start">
-      <a class="navbar-item" href="#">Home</a>
-      <a class="navbar-item" href="#">Shop</a>
-      <a class="navbar-item" href="#">About</a>
-      <a class="navbar-item" href="#">Login</a>
+
+
+
+    <div id="navbarMenu" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="home.php">Home</a>
+        <a class="navbar-item" href="catalogue.php">Shop</a>
+        <a class="navbar-item" href="#">About</a>
+
+        <?php
+
+        if (isset($_SESSION['current_user'])) {
+          if ($_SESSION['current_user'] == 'admin') {
+            echo '
+            <a class="navbar-item" href="accounts.php">Accounts</a>
+            ';
+          }
+        }
+
+        ?>
+      </div>
+      <div class="navbar-end">
+        <?php
+        if(isset($_SESSION['current_user'])) {
+          echo'
+          <div class="navbar-item is-hidden-mobile">
+          <div class="field is-grouped">
+          <a class="button is-inverted is-info" href="profile.php">
+          <span class="icon">
+          <i class="fas fa-user"></i>
+          </span>
+          <span>' . ucfirst($_SESSION['current_user']) . '</span>
+          </a>
+          <a class="button is-danger" href="signout.php">
+          <span class="icon">
+          <i class="fas fa-sign-out-alt"></i>
+          </span>
+          </a>
+          </div>
+          </div>
+          ';
+        } else {
+          echo '
+          <div class="navbar-item is-hidden-mobile">
+            <div class="field is-grouped is-outlined">
+              <p class="control">
+                <a class="button is-primary is-inverted" href="signin.php">
+                  <strong>Sign In</strong>
+                </a>
+              </p>
+              <p class="control has-text-white">or</p>
+              <p class="control">
+                <a class="button is-primary" href="signup.php">
+                  <strong>Sign Up</strong>
+                </a>
+              </p>
+            </div>
+          </div>
+          ';
+        }
+        ?>
+        <span class="navbar-item is-hidden-mobile">
+          <a class="button is-white is-outlined is-small" href="basket.php">
+            <span class="icon">
+              <i class="fas fa-shopping-basket"></i>
+            </span>
+            <span>Shopping Basket</span>
+            <span id="basket-badge">
+              <span class="my-badge">
+                <?php
+                if (isset($_SESSION['basket_count'])) {
+                  echo ''.$_SESSION['basket_count'].'';
+                }
+                ?>
+              </span>
+            </span>
+          </a>
+        </span>
+      </div>
     </div>
-  </div>
-</nav> -->
+  </nav>
 
 
 <section class="hero is-dark is-bold">
- <div class="hero-head">
-   <nav class="navbar">
-     <div class="container">
-       <div class="navbar-brand is-inverted">
-         <a class="navbar-item" href="index.php">
-           <img src="assets/img/logo.jpg" alt="Logo">
-         </a>
-         <span class="navbar-burger burger" data-target="navbarMenu">
-           <span></span>
-           <span></span>
-           <span></span>
-         </span>
-       </div>
-       <div id="navbarMenu" class="navbar-menu">
-         <div class="navbar-start">
-           <a class="navbar-item" href="home.php">Home</a>
-           <a class="navbar-item" href="catalogue.php">Shop</a>
-           <a class="navbar-item" href="#">About</a>
 
-           <?php
+  <div class="hero-head">
 
-           if (isset($_SESSION['current_user'])) {
-             if ($_SESSION['current_user'] == 'admin') {
-               echo '
-               <a class="navbar-item" href="accounts.php">Accounts</a>
-               ';
-             }
-           }
-
-            ?>
-         </div>
-         <div class="navbar-end">
-             <?php
-             if(isset($_SESSION['current_user'])) {
-              echo'
-                <div class="navbar-item">
-                <div class="field is-grouped">
-                <a class="button is-inverted is-info" href="profile.php">
-                <span class="icon">
-                <i class="fas fa-user"></i>
-                </span>
-                <span>' . ucfirst($_SESSION['current_user']) . '</span>
-                </a>
-                <a class="button is-danger" href="signout.php">
-                <span class="icon">
-                <i class="fas fa-sign-out-alt"></i>
-                </span>
-                </a>
-                </div>
-                </div>
-                ';
-                } else {
-                  echo '
-                  <div class="navbar-item">
-                  <div class="field is-grouped is-outlined">
-                  <p class="control">
-                  <a class="button is-primary is-inverted" href="signin.php">
-                  <strong>Sign In</strong>
-                  </a>
-                  </p>
-                  <p class="control has-text-white">or</p>
-                  <p class="control">
-                  <a class="button is-primary" href="signup.php">
-                  <strong>Sign Up</strong>
-                  </a>
-                  </p>
-                  </div>
-                  </div>
-                  ';
-                }
-                ?>
-           <span class="navbar-item">
-             <a class="button is-white is-outlined is-small" href="basket.php">
-               <span class="icon">
-                 <i class="fas fa-shopping-basket"></i>
-               </span>
-               <span>Shopping Basket</span>
-                 <span id="basket-badge">
-                  <span class="my-badge">
-                   <?php
-                   if (isset($_SESSION['basket_count'])) {
-                     echo ''.$_SESSION['basket_count'].'';
-                   }
-                   ?>
-                 </span>
-                </span>
-             </a>
-           </span>
-         </div>
-       </div>
-   </nav>
-
+  </div>
 
  <div class="hero-body">
    <div class="container has-text-centered">
